1656221fae0f02331221b508b1c0c543d0e066bc
Victor Olsson <victor.olsson@leovegas.com>
2020-12-02 10:08:26 +0100

    Init repo

---
+++ template.js
@@ -0,0 +1,14 @@
+const solvePuzzle = (input) => {
+  
+}
+
+const CORRECT_ANSWER = undefined;
+const INPUT = '';
+
+const startDate = new Date();
+const result = solvePuzzle(INPUT);
+console.log('--- Puzzle result ---\n');
+console.log(result);
+console.log(result === CORRECT_ANSWER ? 'Test success' : 'Test fail');
+console.log('Run time:', (new Date() - startDate)/1000, 'seconds');
+console.log('\n---------------------');
